8cd44c5b5fb2ef52f27a0a7661f2f964cdf5b1a1
Ben <benduke.nz@gmail.com>
2018-04-05 15:16:37 +1200

    Last sync test

--- Assignment/src/GameEngine.java
+++ Assignment/src/GameEngine.java
@@ -4,6 +4,7 @@ public class GameEngine {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello tiddles");
+		System.out.println("Yay end of the day");
 	}
 
 }
