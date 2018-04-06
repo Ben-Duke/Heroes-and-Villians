1581bbc57233927f8736b2e6c38d290bf5504b73
Ben <benduke.nz@gmail.com>
2018-04-06 14:59:34 +1200

    Added Classes
    Added City, Villian
    Updated GameEngine.

---
+++ Assignment/src/Villian.java
@@ -0,0 +1,12 @@
+
+public class Villian {
+	private String name;
+	private int villainLives;
+	private int villainDamage;
+	private String villainFaveGame;
+	Villian(String newName, int newLives, int damage){
+		name = newName;
+		villainLives = newLives;
+		villainDamage = damage;
+	}
+}
