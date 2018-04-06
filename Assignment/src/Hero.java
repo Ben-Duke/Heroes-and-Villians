2f6ba36c05368a47f262fa5edaad3e3053823019
Angelica Dela Cruz <angelicadela.cruz@yahoo.com>
2018-04-06 15:00:13 +1200

    Added Class Hero
    The not finished version

--- Assignment/src/Hero.java
+++ Assignment/src/Hero.java
@@ -5,13 +5,34 @@ import java.util.ArrayList;
  *
  */
 public class Hero {
-	private ArrayList<Item> heroInventory;
-	private double heroHealth = 100.0;
-	private ArrayList<String> heroAbilities;
-	private ArrayList<String> powerupList;
+	private String[] heroTypes = {"Earth", "Air", "Water", "Fire"};
+	private int maxHealth = 100;
+	private int currentHealth = 100;
+	private String heroType;
+	private String heroAbility;
+	private ArrayList<String> heroPowerup;
+	private ArrayList<String> heroHealingItems; 
+	private ArrayList<String> heroMapList;
+	private boolean heroStatus = true;
 	
-	public increasehealth()
+	public void increaseHealth(int health) {
+		if(currentHealth < maxHealth) {
+			currentHealth += health;
+		}
+	}
+	public void decreaseHealth(int damage) {
+		if(currentHealth < maxHealth) {
+			currentHealth -= damage;
+		}
+		
+		if(currentHealth < 1) {
+			heroStatus = false;
+		}
+	}
 	
+	public void setType(string type) {
+		if()
+	}
 	
 	
 	
