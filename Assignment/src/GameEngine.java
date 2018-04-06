aadaaf1fe32792fb8001f47ae843e1cbf4b00a03
Ben <benduke.nz@gmail.com>
2018-04-06 13:51:47 +1200

    Uppdated Game Engine with looping logic
    Added base idea for gameloop

--- Assignment/src/GameEngine.java
+++ Assignment/src/GameEngine.java
@@ -1,8 +1,26 @@
-
+import java.util.Scanner;
 public class GameEngine {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		boolean exit = false;
+		
+		
+		while (exit == false) {
+			Scanner reader = new Scanner(System.in);
+			System.out.println("Press q to exit");
+			String i = reader.nextLine();
+			System.out.println(i);
+
+			if(i.equals("q")) {
+				System.out.println("Exiting");
+				exit = true;
+				reader.close();
+			}
+			else {
+				System.out.println("Else happend");
+			}
+		}
 		System.out.println("Hello tiddles");
 		System.out.println("Yay end of the day");
 	}
