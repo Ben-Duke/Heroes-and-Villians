b80a78d6e533e12f37d5bd1a914c2f88d2c77478
Angelica Dela Cruz <angelicadela.cruz@yahoo.com>
2018-04-16 15:22:32 +1200

    Class HomeBase added

---
+++ Assignment/src/HomeBase.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class HomeBase {
+	private ArrayList<Hero> teamHeroes = new ArrayList<Hero>();
+
+	public String viewHeroAttributes() {
+		for(Hero hero: teamHeroes) {
+			System.out.println(hero + "The attributes of the Hero is" + hero.getAbility());
+		}
+		
+	}
+	
+	public boolean viewHeroStatus() {
+		if()
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
