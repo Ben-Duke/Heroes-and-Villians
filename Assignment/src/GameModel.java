bcca72e62db1379f2de6c1dd231feadd0d4cb16b
Angelica Dela Cruz <angelicadela.cruz@yahoo.com>
2018-05-25 13:39:41 +1200

    JavaDoc for Classes

--- Assignment/src/GameModel.java
+++ Assignment/src/GameModel.java
@@ -1,53 +1,120 @@
 import java.util.ArrayList;
 
+/**
+ * This class implements a Game Model that stores references for the game and hold the core references
+ * for the Game Plays. 
+ * @author Ben Duke & Angelica Dela Cru
+ * @version 1.2, May 2018.
+ */
 public class GameModel {
+	/**
+	 * The Team in the Game
+	 */
+	private Team game_team = null;
+	/**
+	 * The counts of the City
+	 */
+	private int cityCount = 1;
+	/**
+	 * The Game X and O'ss
+	 */
+	private XAndOs XAndOgame;
+	/**
+	 * The list of the City
+	 */
+	private ArrayList<City> citylist; 
+	/**
+	 * Initializes the Shop
+	 */
+	private Shop shop = new Shop();
+	/**
+	 * The current destination list
+	 */
+	private Destination[] current;
+	/**
+	 * The current City
+	 */
+	private int currentCity = 0;
 	
-	Team game_team = null;
-	int cityCount = 1;
-	XAndOs XAndOgame;
-	ArrayList<City> citylist; 
-	Shop shop = new Shop();
-	Destination[] current;
-	int currentCity = 0;
-	
-	void moveCity() {
+	/**
+	 * This function moves the City to the next City
+	 */
+	public void moveCity() {
 		currentCity ++;
 	}
 	
-	boolean createTeam(String name) {
+	/**
+	 * This function creates the Team for the Game.
+	 * @param name The String representation of the name of the Team
+	 * @return Returns a boolean value true if the Team is created
+	 */
+	public boolean createTeam(String name) {
 		game_team = new Team();
 		game_team.setTeamName(name);
 		return true;
 	}
 	
-	void updateCityCount(int numOfCity) {
+	/**
+	 * This function updates the number of the City Count
+	 * @param numOfCity The Integer value that the city count will be changed to.
+	 */
+	public void updateCityCount(int numOfCity) {
 		cityCount = numOfCity;
 	}
 	
-	Shop getShop() {
+	/**
+	 * This function returns the Shop in the City
+	 * @return Returns the Shop
+	 */
+	public Shop getShop() {
 		return shop;
 	}
 	
-	Team getTeam() {
+	/**
+	 * This function returns the Team of the Game
+	 * @return Returns the Team
+	 */
+	public Team getTeam() {
 		return game_team;
 	}
 	
-	void createXAndOGame() {
+	/**
+	 * This function initializes the game X and O's
+	 */
+	public void createXAndOGame() {
 		XAndOgame = new XAndOs();
 	}
 	
-	XAndOs getXAndOGame() {
+	/**
+	 * This function returns the game X and O's
+	 * @return Returns the X and O's game 
+	 */
+	public XAndOs getXAndOGame() {
 		return XAndOgame;
 	}
 	
-	ArrayList<City> getCities() {
+	/**
+	 * This function returns the Array List of the Cities
+	 * @return Returns the list of Cities
+	 */
+	public ArrayList<City> getCities() {
 		return citylist;
 	}
 	
-	int getCurrentCity() {
+	/**
+	 * This function returns the Integer value of the Current City
+	 * @return Returns the Integer value of the City at the time
+	 */
+	public int getCurrentCity() {
 		return currentCity;
 	}
-	void setCities(int numberOfCities) {
+	
+	/**
+	 * This function sets the number of Cities in the Game.
+	 * @param numberOfCities An Integer value that is used to set the number of Cities 
+	 * in the Game to.
+	 */
+	public void setCities(int numberOfCities) {
 		ArrayList<City> templist = new ArrayList<City>();
 		
 		for (int i =0;i< numberOfCities; i ++ ) {
@@ -62,14 +129,13 @@ public class GameModel {
 			
 			templist.add(tempcity);
 		}
-		
 		citylist = templist;
 	}
 	
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-	}
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		
+//	}
 	
 }
